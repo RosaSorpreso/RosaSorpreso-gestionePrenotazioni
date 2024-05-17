e54c2b79376d0e259af0e6daeb34bdc692609458
RosaSorpreso <rosasorpreso@gmail.com>
2024-05-17 10:45:25 +0200

    creazione progetto

---
+++ src/test/java/com/bookingManagment/BookingManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
